719a0f4862189fe8af9b5fab90ccd49587232a0f
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-01-28 21:15:32 +0000

    feat(BlogPage): Integrate Blog component and fetch individual blog data

--- src/pages/blogs/BlogPage.js
+++ src/pages/blogs/BlogPage.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Blog from "./Blog";
 
 function BlogPage() {
   const { category, id} = useParams();
@@ -35,7 +36,7 @@ function BlogPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Blog component</p>
+        <Blog {...blog.results[0]} setBlogs={setBlog} blogPage />
         <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
