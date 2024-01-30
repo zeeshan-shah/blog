527d5349b7b52869ddee2ae22e3e10b4134afc10
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-01-30 20:58:58 +0000

    feat(Blogs): Implement BlogsPage component

---
+++ src/pages/blogs/BlogsPage.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import Blog from "./Blog";
+import Asset from "../../components/Asset";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/BlogsPage.module.css";
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
+import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+
+function BlogsPage({ message, filter = "" }) {
+  const [blogs, setBlogs] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+  const { category } = useParams();
+
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        let url;
+        if (category) {
+          // If category is defined
+          url = `/blogs/${category}/?${filter}search=${query}`;
+        } else {
+          // If category is not defined, fetch data from all categories with filter
+          const allCategories = ["science", "politics", "sports", "travel", "programming"];
+          const categoryData = await Promise.all(
+            allCategories.map(async (cat) => {
+              const { data } = await axiosReq.get(`/blogs/${cat}/?${filter}search=${query}`);
+              return data.results;
+            })
+          );
+
+          // Combine results from all categories
+          const combinedData = categoryData.reduce((acc, curr) => acc.concat(curr), []);
+          setBlogs({ results: combinedData });
+          setHasLoaded(true);
+          return; // exit early
+        }
+
+        const { data } = await axiosReq.get(url);
+        setBlogs(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchBlogs();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname, category]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search blogs"
+          />
+        </Form>
+
+        {hasLoaded ? (
+          <>
+            {blogs.results.length ? (
+              <InfiniteScroll
+                children={blogs.results.map((blog) => (
+                  <Blog key={blog.id} {...blog} setBlogs={setBlogs} />
+                ))}
+                dataLength={blogs.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!blogs.next}
+                next={() => fetchMoreData(blogs, setBlogs)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
+}
+
+export default BlogsPage;
