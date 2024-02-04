53b7da76c219a6fd0ce51fa85dfac0c516a5f55e
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-04 22:14:14 +0000

    style: Update import path and class name in NotFoundPage.js

---
+++ src/pages/notfoundpage/NotFoundPage.js
@@ -0,0 +1,16 @@
+// NotFoundPage.js
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../../styles/NotFoundPage.module.css"; // Update the import path
+
+const NotFoundPage = () => {
+  return (
+    <div className={styles.notFoundContainer}> {/* Update the class name */}
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you are looking for might be under construction or does not exist.</p>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
