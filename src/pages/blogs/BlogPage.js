0aa01e8e566ba216ef09fec63ca66da4ac85369b
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-04 22:09:31 +0000

    feat: Implement BlogPage component for displaying individual blog details

--- src/pages/blogs/BlogPage.js
+++ src/pages/blogs/BlogPage.js
@@ -1,46 +1,91 @@
 import React, { useEffect, useState } from "react";
-
+import { useHistory, useParams } from "react-router";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Blog from "./Blog";
+import Comment from "../comments/Comment";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function BlogPage() {
-  const { category, id} = useParams();
-  
+  const { category, id } = useParams();
+  const history = useHistory();
+
   const [blog, setBlog] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: blog }] = await Promise.all([
+        const [{ data: blog }, { data: comments }] = await Promise.all([
           axiosReq.get(`/blogs/${category}/${id}`),
+          axiosReq.get(`/comments/?blog=${id}`),
         ]);
+
         setBlog({ results: [blog] });
-        console.log(blog);
-        console.log("Category:", category);
-        console.log("ID:", id);
+        setComments(comments);
       } catch (err) {
-        console.log(err);
+        console.error(err);
+        // Redirect to the home page if an error occurs (blog not found)
+        history.push("/");
       }
     };
 
     handleMount();
-  }, [category, id]);
+  }, [category, id, history]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Blog {...blog.results[0]} setBlogs={setBlog} blogPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              blog={id}
+              setBlog={setBlog}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setBlog={setBlog}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <PopularProfiles />
       </Col>
     </Row>
   );
