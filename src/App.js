71fa750b346da62d8d9dadc0694565103c887f86
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-04 22:19:39 +0000

    refactor: Organize route rendering in App component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ import BlogCreateForm from "./pages/blogs/BlogCreateForm";
 import BlogPage from "./pages/blogs/BlogPage";
 import BlogsPage from "./pages/blogs/BlogsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
+import BlogEditForm from "./pages/blogs/BlogEditForm";
+import ProfilePage from "./pages/profiles/ProfilePage";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import Category from "./pages/blogs/Category";
+import Home from "./pages/home/HomePage";
+import UpcomingBlogsCreateForm from "./pages/upcomingblogs/UpcomingBlogsCreateForm";
+import UpcomingBlogs from "./pages/upcomingblogs/UpcomingBlogs";
+import UpcomingBlogsEditForm from "./pages/upcomingblogs/UpcomingBlogsEditForm";
+import NotFoundPage from "./pages/notfoundpage/NotFoundPage";
+import ContactPage from "./pages/contact/ContactPage";
+import ContactTicketEditPage from "./pages/contact/ContactTicketEditPage";
+
 
 function App() {
   const currentUser = useCurrentUser();
@@ -19,10 +33,16 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/" render={() => <Home /> } />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/blogs/create" render={() => <BlogCreateForm />} />
+          <Route exact path="/upcoming-blogs/create" render={() => <UpcomingBlogsCreateForm />} />
+          <Route exact path="/upcoming-blogs" render={() => <UpcomingBlogs /> } />
+          <Route exact path="/upcoming-blogs/:id/edit" render={() => <UpcomingBlogsEditForm /> } />
+          <Route exact path="/tickets" render={() => <ContactPage /> } />
+          <Route exact path="/tickets/:id/edit" render={() => <ContactTicketEditPage /> } />
+          <Route exact path="/blogs/:cat/:id/edit" render={() => <BlogEditForm />} />
           <Route exact path="/blogs/:category/:id" render={() => <BlogPage />} />
           <Route exact path="/blogs/:category/" render={() => (
               <BlogsPage message="No results found. Adjust the search keyword." />
@@ -35,7 +55,24 @@ function App() {
               />
             )}
           />
-          <Route render={() => <p>Page not found!</p>} />
+          <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+          <Route
+            exact
+            path="/profiles/:id/edit/username"
+            render={() => <UsernameForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit/password"
+            render={() => <UserPasswordForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit"
+            render={() => <ProfileEditForm />}
+          />
+          <Route exact path="/blogs/" render={() => <Category />} />
+          <Route render={() => <NotFoundPage />} />
         </Switch>
       </Container>
     </div>
