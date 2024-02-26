14cd3fa6b6c1dde47e3f2f86db8250751ddd18b8
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 15:31:21 +0000

    feat: Create Asset component

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Spinner } from "react-bootstrap";
-import styles from "../styles/Asset.module.css";
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import styles from '../styles/Asset.module.css';
 
 const Asset = ({ spinner, src, message }) => {
   return (
