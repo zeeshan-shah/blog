0f8d884f07ab465e8224a741ef0a5fbd1f1180e7
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 15:35:45 +0000

    feat: Implement useRedirect hook for automatic redirection based on user authentication status

--- src/hooks/useRedirect.js
+++ src/hooks/useRedirect.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-import { useEffect } from "react";
-import { useHistory } from "react-router";
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
 
 export const useRedirect = (userAuthStatus) => {
   const history = useHistory();
@@ -8,15 +8,15 @@ export const useRedirect = (userAuthStatus) => {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        await axios.post("/dj-rest-auth/token/refresh/");
+        await axios.post('/dj-rest-auth/token/refresh/');
         // if user is logged in, the code below will run
-        if (userAuthStatus === "loggedIn") {
-          history.push("/");
+        if (userAuthStatus === 'loggedIn') {
+          history.push('/');
         }
       } catch (err) {
         // if user is not logged in, the code below will run
-        if (userAuthStatus === "loggedOut") {
-          history.push("/");
+        if (userAuthStatus === 'loggedOut') {
+          history.push('/');
         }
       }
     };
