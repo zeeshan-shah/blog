078d1d3f69ca20ab711e7e30447f61167f3f1931
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 16:08:37 +0000

    feat: Add utility functions for handling JWT tokens and API requests

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
-import { jwtDecode } from "jwt-decode";
-import { axiosReq } from "../api/axiosDefaults";
+import { jwtDecode } from 'jwt-decode';
+import { axiosReq } from '../api/axiosDefaults';
 
 export const fetchMoreData = async (resource, setResource) => {
   try {
@@ -26,12 +26,12 @@ export const followHelper = (profile, clickedProfile, following_id) => {
         following_id,
       }
     : profile.is_owner
-    ? // This is the profile of the logged in user
-      // update its following count
-      { ...profile, following_count: profile.following_count + 1 }
-    : // this is not the profile the user clicked on or the profile
-      // the user owns, so just return it unchanged
-      profile;
+      ? // This is the profile of the logged in user
+        // update its following count
+        { ...profile, following_count: profile.following_count + 1 }
+      : // this is not the profile the user clicked on or the profile
+        // the user owns, so just return it unchanged
+        profile;
 };
 
 export const unfollowHelper = (profile, clickedProfile) => {
@@ -44,23 +44,23 @@ export const unfollowHelper = (profile, clickedProfile) => {
         following_id: null,
       }
     : profile.is_owner
-    ? // This is the profile of the logged in user
-      // update its following count
-      { ...profile, following_count: profile.following_count - 1 }
-    : // this is not the profile the user clicked on or the profile
-      // the user owns, so just return it unchanged
-      profile;
+      ? // This is the profile of the logged in user
+        // update its following count
+        { ...profile, following_count: profile.following_count - 1 }
+      : // this is not the profile the user clicked on or the profile
+        // the user owns, so just return it unchanged
+        profile;
 };
 
 export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
-  localStorage.setItem("refreshTokenTimestamp", refreshTokenTimestamp);
+  localStorage.setItem('refreshTokenTimestamp', refreshTokenTimestamp);
 };
 
 export const shouldRefreshToken = () => {
-  return !!localStorage.getItem("refreshTokenTimestamp");
+  return !!localStorage.getItem('refreshTokenTimestamp');
 };
 
 export const removeTokenTimestamp = () => {
-  localStorage.removeItem("refreshTokenTimestamp");
+  localStorage.removeItem('refreshTokenTimestamp');
 };
