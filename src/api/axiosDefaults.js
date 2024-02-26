a53b937b653f80244a04444735b769d68bb8f488
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 15:29:05 +0000

    feat: Configure Axios defaults

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
 
-axios.defaults.baseURL = "https://pp5-api-e56cee5dddfa.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.baseURL = 'https://pp5-api-e56cee5dddfa.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
