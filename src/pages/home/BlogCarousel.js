004ad86827006ec0626496d6c8979cb8cd4c945f
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-02-26 15:53:47 +0000

    feat: Implement BlogCarousel component for displaying blog posts in a carousel

--- src/pages/home/BlogCarousel.js
+++ src/pages/home/BlogCarousel.js
@@ -1,16 +1,15 @@
-import React from "react";
-import Slider from "react-slick";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Slider from 'react-slick';
+import { Link } from 'react-router-dom';
 
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import styles from "../../styles/BlogCarousel.module.css";
-import buttonStyles from "../../styles/Button.module.css";
-import classNames from "classnames";
-
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import styles from '../../styles/BlogCarousel.module.css';
+import buttonStyles from '../../styles/Button.module.css';
+import classNames from 'classnames';
 
 const BlogCarousel = ({ category, blogs }) => {
   const settings = {
@@ -33,7 +32,12 @@ const BlogCarousel = ({ category, blogs }) => {
 
   return (
     <div className={styles.container}>
-      <h2 className={`${styles.categoryHeading} text-center mb-4`} style={{ color: '#9B2915' }}>{category}</h2>
+      <h2
+        className={`${styles.categoryHeading} text-center mb-4`}
+        style={{ color: '#9B2915' }}
+      >
+        {category}
+      </h2>
       <Slider {...settings}>
         {blogs.map((blog) => (
           <div key={blog.id}>
@@ -43,14 +47,19 @@ const BlogCarousel = ({ category, blogs }) => {
                 src={blog.image}
                 alt={blog.title}
                 className={styles.cardImage}
-                style={{ height: "200px", objectFit: "cover" }}
+                style={{ height: '200px', objectFit: 'cover' }}
               />
               <Card.Body className={styles.customCardBody}>
-                <Card.Title className={styles.cardTitle}>{blog.title}</Card.Title>
+                <Card.Title className={styles.cardTitle}>
+                  {blog.title}
+                </Card.Title>
                 <div className="d-flex justify-content-center align-items-center">
                   <Link to={`/blogs/${category}/${blog.id}`}>
                     <Button
-                      className={classNames(buttonStyles.Button, buttonStyles.Blue)}
+                      className={classNames(
+                        buttonStyles.Button,
+                        buttonStyles.Blue,
+                      )}
                       variant="primary"
                     >
                       Read More
