3f67c6e65bf2cfb1f74827067e8c08f2d168cbd9
Zeeshan Shah <zeeshan_shah200750@yahoo.com>
2024-03-02 17:56:14 +0000

    refactor: Refactor App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import { test } from '@testing-library/react';
+import { expect } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
